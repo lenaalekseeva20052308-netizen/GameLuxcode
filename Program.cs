6a175d59872ee56f8cb3aa2c5a79fdcd79d2e1ed
Anna <Anna@FBI-NIL-ASPIRE5>
2026-08-02 21:02:30 +0700

    представление героя

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Как звоут героя?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Сколько тебе лет?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Твое звание: воин, лучник, маг, житель");
+            string classHero = Console.ReadLine();
+
+            Console.WriteLine("Какова твоя сила?");
+            int strength = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Сколько у тебя золота?");
+            int gold = Convert.ToInt32(Console.ReadLine());
+
+            int health = 100;
+
+            Console.WriteLine($"Привет, {name}! Ты {classHero} в возрасте {age} лет. Сила: {strength}, Золото: {gold}, Здоровье = 100");
         }
     }
 }
